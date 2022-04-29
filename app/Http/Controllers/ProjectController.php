b76f0c2b63656392729234f0166c70f497d02007
Ken <ken.saetern@plumbingmail.com>
2022-04-29 13:18:44 -0700

    Passed all unit tests

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -10,7 +11,12 @@ class ProjectController extends Controller
     {
         // TASK: Add one sentence to save the project to the logged-in user
         //   by $request->project_id and with $request->start_date parameter
-
+            // $projectId = $request->project_id;
+            // $startDate = $request->start_date;
+            // $user = User::find(auth()->user()->id);
+            // $project = Project::find($projectId);
+            // $user->projects()->save($project, ['start_date' => $startDate]);
+        auth()->user()->projects()->save(Project::find($request->project_id), ['start_date' => $request->start_date]);
         return 'Success';
     }
 }
